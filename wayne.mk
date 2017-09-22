3cbf93643aa8add698c69f316d68edd7c0b5420a
Yifan Hong <elsk@google.com>
2017-09-22 15:16:18 -0700

    wayne-common: Uprev to health@2.0 hal.
    Test: adb logcat -e health
    Test: lshal --debug
    
    Change-Id: I3b14f087d95578d034898600592cb6fb357a9958

--- wayne.mk
+++ wayne.mk
@@ -233,8 +233,6 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
 # HIDL
